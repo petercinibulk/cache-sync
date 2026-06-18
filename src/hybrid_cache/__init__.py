415218066a2aa65910fc7ab34484409bb20bcd0b
Peter Cinibulk <petercinibulk@gmail.com>
2026-06-18 05:28:53 -0400

    Export backplane and invalidator APIs

--- src/hybrid_cache/__init__.py
+++ src/hybrid_cache/__init__.py
@@ -1,19 +1,25 @@
-from hybrid_cache.core import (
-    CachedFunction,
-    CacheOptions,
-    DistributedCache,
-    HybridCache,
-    PickleSerializer,
-    Serializer,
-    cached,
-)
+from hybrid_cache.backplane import Backplane, BackplaneMessage
+from hybrid_cache.core import CacheOptions, HybridCache
+from hybrid_cache.decorators import CachedFunction, cached
+from hybrid_cache.distributed_cache import DistributedCache, PickleSerializer, Serializer
+from hybrid_cache.invalidator import BackplaneInvalidator, Invalidator
+from hybrid_cache.redis_backplane import RedisStreamsBackplane
+from hybrid_cache.redis_cache import RedisDistributedCache
+from hybrid_cache.redis_invalidator import RedisInvalidator
 
 __all__ = [
+    "Backplane",
+    "BackplaneInvalidator",
+    "BackplaneMessage",
     "CacheOptions",
     "CachedFunction",
     "DistributedCache",
     "HybridCache",
+    "Invalidator",
     "PickleSerializer",
+    "RedisDistributedCache",
+    "RedisInvalidator",
+    "RedisStreamsBackplane",
     "Serializer",
     "cached",
 ]
